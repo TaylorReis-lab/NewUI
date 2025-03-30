8d5b0ca40851662d16aa45376a498acf27c416d7
Taylor Reis <reist98822@gmail.com>
2025-03-30 12:00:52 -0300

    add arquivos

---
+++ webkit/modules/other_module.js
@@ -0,0 +1,9 @@
+function say_hello(message) {
+    return `Hello ${message}!`
+}
+
+function was_here(message) {
+    return `${message} was here!`
+}
+
+export { say_hello, was_here }
---
+++ webkit/store.js
@@ -0,0 +1,4 @@
+import { say_hello, was_here } from "./modules/other_module.js"
+
+console.log(say_hello("ShadowMonster"));
+console.log(was_here("clawdius"));
